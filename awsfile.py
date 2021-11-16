2974f37389f35cb7da08a975b6754a7d3d01b10a
GPriyankaAWS <94423560+GPriyankaAWS@users.noreply.github.com>
2021-11-16 16:20:08 +0530

    Add files via upload

---
+++ awsfile.py
@@ -0,0 +1,44 @@
+print("Python has three numeric types: int, float, and complex")
+print("hi")
+
+
+myvalue=3.14
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+
+myvalue=5j
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+
+myvalue=True
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+
+myvalue=False
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+
+mystring = "This is a string."
+print(mystring)
+print(type(mystring))
+print(mystring + " is of the data type " + str(type(mystring)))
+
+firststring = "water"
+secondstring = "fall"
+thirdstring = firststring + secondstring
+print(thirdstring)
+name = input("What is your name? ")
+print(name)
+
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
+
+
+
+
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("Hello, World")
+
+2+2
+print("Python has three numeric types: int, float, and complex")
+
+
